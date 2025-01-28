571506d0c63db6de8620010b436c4e1db577a328
arayik-99 <hovhannisyanara99@gmail.com>
2025-01-28 19:56:04 +0400

    feat: segregated blockchain features

--- Runtime/Assets/Scripts/AuthenticationManager.cs
+++ Runtime/Assets/Scripts/AuthenticationManager.cs
@@ -186,7 +186,7 @@ public class AuthenticationManager : MonoBehaviour
         try
         {
             await _beamContext.Api.AuthService.RegisterDBCredentials(email, password);
-            await _beamContext.Accounts.AddExternalIdentity<SuiIdentity, Web3FederationClient>("");
+            // await _beamContext.Accounts.AddExternalIdentity<SuiIdentity, Web3FederationClient>("");
             await _beamContext.Api.AuthService.Login(email, password);
             await _beamContext.Accounts.Current.SetAlias(userName);
 
