5961b1a0112ab4d8c417127df3be131386919cb0
GeorgianaIonUPD <georgiana.ion@updivision.com>
2025-01-30 15:21:50 +0200

    fix: Remaining tokens bug

--- Runtime/Assets/Scripts/DisplaySenetAmount.cs
+++ Runtime/Assets/Scripts/DisplaySenetAmount.cs
@@ -8,12 +8,21 @@ public class DisplaySenetAmount : MonoBehaviour
     [SerializeField]
     private bool isRemainingAfterPayment = false;
 
-    void Update()
+    private double _remainingSenetAmount;
+
+    private void Start()
     {
         if (CurrencyManager.instance)
         {
-            if (isRemainingAfterPayment) _senetAmount.text = $"{CurrencyManager.instance.senet - 25}";
-            else _senetAmount.text = $"{CurrencyManager.instance.senet}";
+            _remainingSenetAmount = CurrencyManager.instance.senet - 25;
+        }
+    }
+
+    private void Update()
+    {
+        if (CurrencyManager.instance)
+        {
+            _senetAmount.text = isRemainingAfterPayment ? $"{_remainingSenetAmount}" : $"{CurrencyManager.instance.senet}";
         }
     }
 }
